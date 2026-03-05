38d16736538c0c23878f1e9f046ca6c8ab382c22
Willian dos Santos Silva <86166759+Wiillboss@users.noreply.github.com>
2026-03-05 20:12:40 -0300

    finalizando projeto relógio

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//selecionando elementos que deveram ser usados
 let digitalElement = document.querySelector('.digital');
 let sElement = document.querySelector('.p_s');
 let mElement = document.querySelector('.p_m');
@@ -9,9 +10,15 @@ function updateClock() {
     let minute = now.getMinutes();
     let second = now.getSeconds();
 
-    digitalElement.innerHTML = `${fixZero(hour)}: ${fixZero(minute)}: ${fixZero(second)}`;
+    digitalElement.innerHTML = `${fixZero(hour)}: ${fixZero(minute)}: ${fixZero(second)}`; // deixando sempre com dois digitos
 
-    
+    let sDeg = ((360 / 60) * second) - 90;
+    let mDeg =  ((360 / 60) * minute) - 90;
+    let hDeg =  ((360 / 12) * hour) - 90;
+
+    sElement.style.transform = `rotate(${sDeg}deg)`;
+    mElement.style.transform = `rotate(${mDeg}deg)`;
+    hElement.style.transform = `rotate(${hDeg}deg)`;
 }
 
 function fixZero(time){
@@ -26,3 +33,4 @@ function fixZero(time){
 }
 
 setInterval(updateClock, 1000);
+updateClock();
